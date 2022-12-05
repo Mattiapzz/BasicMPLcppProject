df5a11136fffe3abde7349edfeb287ffbc63b57c
Mattia Piazza <mattia.piazza@unitn.it>
2022-12-05 12:12:27 +0100

    add basic class and Utils submodules

---
+++ src/BasicMPLcppProject/MPL.hxx
@@ -0,0 +1,63 @@
+/*
+(***********************************************************************************)
+(*                                                                                 *)
+(* The BASICMPLCPPPROJECT project                                                  *)
+(*                                                                                 *)
+(* Copyright (c) 2020, Mattia Piazza, Mattia Piccininit, Enrico Bertolazzi and     *)
+(* Francesco Biral                                        )                        *)
+(*                                                                                 *)
+(* The BASICMPLCPPPROJECT project and its components are supplied under the terms  *)
+(* of the open source BSD 2-Clause License. The contents of the BASICMPLCPPPROJECT *)
+(* project and its components may not be copied or disclosed except in             *)
+(* accordance with the terms of the BSD 2-Clause License.                          *)
+(*                                                                                 *)
+(* URL: https://opensource.org/licenses/BSD-2-Clause                               *)
+(*                                                                                 *)
+(*    Mattia Piazza                                                                *)
+(*    Department of Industrial Engineering                                         *)
+(*    University of Trento                                                         *)
+(*    e-mail: mattia.piazza@unitn.it                                               *)
+(*                                                                                 *)
+(***********************************************************************************)
+*/
+
+///
+/// file: MPL.hh
+///
+
+#pragma once
+
+#ifndef INCLUDE_MPL
+#define INCLUDE_MPL
+
+namespace mpl
+{
+
+  //! class1 Base class container
+  /**
+   * Speed Matching Motion Primitives
+   */
+  class class1 
+  {
+  private:
+
+  public:
+
+    //! class1 copy constructor
+    class1(class1 const &) = default;
+
+    ~class1(void) = default;
+
+    //! SpeedMatching class constructor
+    class1(void);
+
+    
+  }; // class class1
+
+} // namespace mp
+
+#endif
+
+///
+/// eof: MPL.hh
+///
---
+++ src/MPL.cc
@@ -0,0 +1,41 @@
+/*
+(***********************************************************************************)
+(*                                                                                 *)
+(* The BASICMPLCPPPROJECT project                                                  *)
+(*                                                                                 *)
+(* Copyright (c) 2020, Mattia Piazza, Mattia Piccininit, Enrico Bertolazzi and     *)
+(* Francesco Biral                                        )                        *)
+(*                                                                                 *)
+(* The BASICMPLCPPPROJECT project and its components are supplied under the terms  *)
+(* of the open source BSD 2-Clause License. The contents of the BASICMPLCPPPROJECT *)
+(* project and its components may not be copied or disclosed except in             *)
+(* accordance with the terms of the BSD 2-Clause License.                          *)
+(*                                                                                 *)
+(* URL: https://opensource.org/licenses/BSD-2-Clause                               *)
+(*                                                                                 *)
+(*    Mattia Piazza                                                                *)
+(*    Department of Industrial Engineering                                         *)
+(*    University of Trento                                                         *)
+(*    e-mail: mattia.piazza@unitn.it                                               *)
+(*                                                                                 *)
+(***********************************************************************************)
+*/
+
+///
+/// file: MPL.cc
+///
+
+#include "BasicMPLcppProject.hh"
+
+namespace mpl
+{
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  class1::class1(void)  {  }
+
+} // namespace mpl
+
+///
+/// eof: MPL.cc
+///
